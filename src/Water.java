6b30074b98c61697781c012f08bc5761cd68acda
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 12:25:05 -0500

    created class Water, added some outputes

---
+++ src/Water.java
@@ -0,0 +1,8 @@
+public class Water {
+    public static void main(String[] args) {
+
+        System.out.println("Alex is drinking water");
+        System.out.println("Inna is drinking coffee");
+
+    }
+}
