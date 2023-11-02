1945d83784087b300d95f747a9e968113bb14f11
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 12:54:40 -0500

    created class Hat, added some lines

---
+++ src/Hat.java
@@ -0,0 +1,9 @@
+public class Hat {
+    public static void main(String[] args) {
+
+        System.out.println("Alex is wearing baseball cap");
+        System.out.println("Inna is wearing nothing");
+
+
+    }
+}
