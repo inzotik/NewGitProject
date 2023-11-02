7d36a4d9edfb09f1323d03d877b3bd63bd13877a
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 12:26:49 -0500

    class Food was created, added some outputes

---
+++ src/Food.java
@@ -0,0 +1,9 @@
+public class Food {
+    public static void main(String[] args) {
+
+        System.out.println("Sasha is eating apple");
+        System.out.println("Inna is eating christmas ravioli");
+
+
+    }
+}
