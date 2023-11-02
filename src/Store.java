0e380b9c84e64c6458e58bbbb4f668d606599217
Inna_Cydeo <innazotikova@gmail.com>
2023-11-02 13:03:53 -0500

    one more line was added

--- src/Store.java
+++ src/Store.java
@@ -2,6 +2,7 @@ public class Store {
     public static void main(String[] args) {
 
         System.out.println("Now we can sell apples:)");
+        System.out.println("So now, Alex can eat apples as much, as he wants.");
 
     }
 }
